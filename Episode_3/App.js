19fe3225a557707e82d0e87e1097fc897a5e1b0d
iAyushCodes <code.ayush.ai@gmail.com>
2025-05-01 20:34:19 +0530

    Learning JSX Syntax

---
+++ Episode_3/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const jsxHeading = <h1 id="heading">Hello Everyone from JSX 🚀</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
