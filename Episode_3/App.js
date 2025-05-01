3f61e10e429a71050d3c9dbec7d9f1179b6e57ca
iAyushCodes <code.ayush.ai@gmail.com>
2025-05-01 21:19:10 +0530

    Created Functional Components

--- Episode_3/App.js
+++ Episode_3/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 id="heading">Hello Everyone from JSX 🚀</h1>
+// JSX Syntax
+// const jsxHeading = <h1 id="heading">Hello Everyone from JSX 🚀</h1>
+
+const TitleComponent = () => <h1 id="title">This is A Title Component</h1>;
+
+const HeadingComponent = () => (
+  // Component Composition -> Component inside another component
+  // Nested JSX
+   <div id="container">
+    <TitleComponent />
+    <h2 id="heading">This Is A Heading Component</h2>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
